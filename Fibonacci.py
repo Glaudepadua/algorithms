6ddf35c2d282da84ba7e138d0b3feffe2eecf6be
glaudepadua <glaudepadua@gmail.com>
2022-08-13 12:39:38 -0300

    Implements recursive fibonacci algorithm

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,22 +1,55 @@
-#Iteractive version
+#Iterative algorithm
+def fibonatti_iterative(num):
+    '''
+    Return the referred value of a position of Fibonacci sequence.
+    Iterative algotithm
 
-def fibonatti_iteractive(n):
+    Parameters:
+        num (int): position number
+
+    Returns:
+        Calculate the value of the given position in Fibonacci sequence
+    '''
+
     previous = 0
     current = 1
     final = 0
 
-    for _ in range(n-1):
+    for _ in range(num-1):
         final = current + previous
         previous = current
         current = final
 
     return final
 
+#Recursive algorithm
+def fibonacci_recursive(num):
+    '''
+    Return the referred value of a position of Fibonacci sequence.
+    Recursive algotithm
+
+    Parameters:
+        num (int): position number
+
+    Returns:
+        Calculate the value of the given position in Fibonacci sequence
+    '''
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    else:
+        return fibonacci_recursive(num-1) + fibonacci_recursive(num-2)
+
 def main():
-    print("Type a number")    
+    print("Type a number")
     num = int(input())
-    result = fibonatti_iteractive(num)
-    print("Referred fibonacci sequence: " + str(result))
+
+    result = fibonatti_iterative(num)
+    print("Referred fibonacci sequence (iteractive): " + str(result))
+
+    result = fibonacci_recursive(num)
+    print("Referred fibonacci sequence (recursive): " + str(result))
 
 if __name__ == "__main__":
     main()
